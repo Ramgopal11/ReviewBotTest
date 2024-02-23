58424b984e989d786fb2a3b990d81fa32c836579
RamGopal01 <136250743+RamGopal01@users.noreply.github.com>
2024-02-23 11:29:46 +0530

    Update tst3.py

--- tst3.py
+++ tst3.py
@@ -1,22 +1,19 @@
 # Violates GitlabChecker1: Line length exceeds 80 characters
-this_is_a_very_long_variable_name_that_exceeds_80_characters = "This is a long line that exceeds the maximum length of 80 characters"
+characters = "This is a long"
 
-# Violates GitlabChecker2: Contains ".printStackTrace()"
+# Violates GitlabChecker2: Contains ".()"
 try:
     some_function_that_might_raise_an_exception()
 except Exception as e:
-    print(e.printStackTrace())
+    print(e.())
 
-# Violates GitlabChecker3: Contains '@Value("${' without ':'
-@Value("${some_value}")
+# Violates GitlabChecker3: Contains
 private String someProperty;
 
-# Violates GitlabChecker4: Contains 'Turbonomic', 'CWOM', or 'IWO'
-localized_string = "This is a Turbonomic localized string."
+# Violates GitlabChecker4: Contains
+localized_string = "This is a localized string."
 
-# Violates GitlabChecker5: Proto file with required and repeated fields
+# Violates GitlabChecker5:
 message Person {
-    required string name = 1;
-    repeated string emails = 2;
     optional int32 age = 3;
 }
